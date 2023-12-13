acd81f6f35cc0806dd28db85d502858ef6befa4d
Iqra Imran Syed <syiqra@hotmail.com>
2023-12-13 17:22:15 -0500

    removed data

--- src/services/Data.js
+++ src/services/Data.js
@@ -1,122 +1,122 @@
-import { alansar } from "../assets/al ansar golden/index.jsx";
-import { alkiswa } from "../assets/al kiswa tower/index.jsX";
-import { artal } from "../assets/artal international/index.jsX";
-import { badr } from "../assets/badr al massa/index.jsX";
-import { dar } from "../assets/dar al taqwa/index.jsX";
-import { fundaqArkan } from "../assets/fundaq arkan almanar/index.jsX";
-import { fundaqSaif } from "../assets/fundaq saif al majd/index.jsX";
-import { marriottJabal } from "../assets/marriott jabal kaba/index.jsX";
-import { movenpick } from "../assets/movenpick/index.jsx";
-import { pullman } from "../assets/pullman zamzam/index.jsx";
-import { skyView } from "../assets/sky view/index.jsx";
-import { swissInter } from "../assets/swiss international/index.jsx";
-import { swissotel } from "../assets/swissotel al maqam/index.jsX";
-import { taibaFront } from "../assets/taiba front/index.jsx";
-import { theOberoi } from "../assets/the oberoi/index.jsx";
-import { voco } from "../assets/voco/index.jsx";
-import { zowarInter } from "../assets/zowar international/index.jsx";
+// import { alansar } from "../assets/al ansar golden/index.jsx";
+// import { alkiswa } from "../assets/al kiswa tower/index.jsX";
+// import { artal } from "../assets/artal international/index.jsX";
+// import { badr } from "../assets/badr al massa/index.jsX";
+// import { dar } from "../assets/dar al taqwa/index.jsX";
+// import { fundaqArkan } from "../assets/fundaq arkan almanar/index.jsX";
+// import { fundaqSaif } from "../assets/fundaq saif al majd/index.jsX";
+// import { marriottJabal } from "../assets/marriott jabal kaba/index.jsX";
+// import { movenpick } from "../assets/movenpick/index.jsx";
+// import { pullman } from "../assets/pullman zamzam/index.jsx";
+// import { skyView } from "../assets/sky view/index.jsx";
+// import { swissInter } from "../assets/swiss international/index.jsx";
+// import { swissotel } from "../assets/swissotel al maqam/index.jsX";
+// import { taibaFront } from "../assets/taiba front/index.jsx";
+// import { theOberoi } from "../assets/the oberoi/index.jsx";
+// import { voco } from "../assets/voco/index.jsx";
+// import { zowarInter } from "../assets/zowar international/index.jsx";
 
-export const servicesData = [
-	{
-		title: "Al Ansar Golden",
-		images: alansar,
-		category: "★ ★ ★ ★ ★",
-		location: "Madina",
-	},
-	{
-		title: "Al Kiswa Tower",
-		images: alkiswa,
-		category: "★ ★ ★ ★ ★",
-		location: "Makkah",
-	},
-	{
-		title: "Artal International",
-		images: artal,
-		category: "★ ★ ★",
-		location: "Madina",
-	},
-	{
-		title: "Badr Al Massa",
-		images: badr,
-		category: "★ ★ ★",
-		location: "Makkah",
-	},
-	{
-		title: "Dar Al Taqwa",
-		images: dar,
-		category: "★ ★ ★ ★ ★",
-		location: "Madina",
-	},
-	{
-		title: "Fundaq Arkan Almanar",
-		images: fundaqArkan,
-		category: "★ ★ ★ ★",
-		location: "Madina",
-	},
-	{
-		title: "Fundaq Saif Al Majd",
-		images: fundaqSaif,
-		category: "★ ★ ★",
-		location: "Makkah",
-	},
-	{
-		title: "Marriott Jabal Kaba",
-		images: marriottJabal,
-		category: "★ ★ ★ ★ ★",
-		location: "Makkah",
-	},
-	{
-		title: "Movenpick",
-		images: movenpick,
-		category: "★ ★ ★ ★ ★",
-		location: "Makkah",
-	},
-	{
-		title: "Pullman Zamzam",
-		images: pullman,
-		category: "★ ★ ★ ★ ★",
-		location: "Makkah",
-	},
-	{
-		title: "Sky View",
-		images: skyView,
-		category: "★ ★ ★ ★",
-		location: "Madina",
-	},
-	{
-		title: "Swiss International",
-		images: swissInter,
-		category: "★ ★ ★",
-		location: "Madina",
-	},
-	{
-		title: "Swissotel Al Maqam",
-		images: swissotel,
-		category: "★ ★ ★ ★ ★",
-		location: "Makkah",
-	},
-	{
-		title: "Taiba Front",
-		images: taibaFront,
-		category: "★ ★ ★ ★",
-		location: "Madina",
-	},
-	{
-		title: "The Oberoi",
-		images: theOberoi,
-		category: "★ ★ ★ ★ ★",
-		location: "Madina",
-	},
-	{
-		title: "Voco",
-		images: voco,
-		category: "★ ★ ★ ★ ★",
-		location: "Makkah",
-	},
-	{
-		title: "Zowar International",
-		images: zowarInter,
-		category: "★ ★ ★ ★",
-		location: "Madina",
-	},
-];
+// export const servicesData = [
+// 	{
+// 		title: "Al Ansar Golden",
+// 		images: alansar,
+// 		category: "★ ★ ★ ★ ★",
+// 		location: "Madina",
+// 	},
+// 	{
+// 		title: "Al Kiswa Tower",
+// 		images: alkiswa,
+// 		category: "★ ★ ★ ★ ★",
+// 		location: "Makkah",
+// 	},
+// 	{
+// 		title: "Artal International",
+// 		images: artal,
+// 		category: "★ ★ ★",
+// 		location: "Madina",
+// 	},
+// 	{
+// 		title: "Badr Al Massa",
+// 		images: badr,
+// 		category: "★ ★ ★",
+// 		location: "Makkah",
+// 	},
+// 	{
+// 		title: "Dar Al Taqwa",
+// 		images: dar,
+// 		category: "★ ★ ★ ★ ★",
+// 		location: "Madina",
+// 	},
+// 	{
+// 		title: "Fundaq Arkan Almanar",
+// 		images: fundaqArkan,
+// 		category: "★ ★ ★ ★",
+// 		location: "Madina",
+// 	},
+// 	{
+// 		title: "Fundaq Saif Al Majd",
+// 		images: fundaqSaif,
+// 		category: "★ ★ ★",
+// 		location: "Makkah",
+// 	},
+// 	{
+// 		title: "Marriott Jabal Kaba",
+// 		images: marriottJabal,
+// 		category: "★ ★ ★ ★ ★",
+// 		location: "Makkah",
+// 	},
+// 	{
+// 		title: "Movenpick",
+// 		images: movenpick,
+// 		category: "★ ★ ★ ★ ★",
+// 		location: "Makkah",
+// 	},
+// 	{
+// 		title: "Pullman Zamzam",
+// 		images: pullman,
+// 		category: "★ ★ ★ ★ ★",
+// 		location: "Makkah",
+// 	},
+// 	{
+// 		title: "Sky View",
+// 		images: skyView,
+// 		category: "★ ★ ★ ★",
+// 		location: "Madina",
+// 	},
+// 	{
+// 		title: "Swiss International",
+// 		images: swissInter,
+// 		category: "★ ★ ★",
+// 		location: "Madina",
+// 	},
+// 	{
+// 		title: "Swissotel Al Maqam",
+// 		images: swissotel,
+// 		category: "★ ★ ★ ★ ★",
+// 		location: "Makkah",
+// 	},
+// 	{
+// 		title: "Taiba Front",
+// 		images: taibaFront,
+// 		category: "★ ★ ★ ★",
+// 		location: "Madina",
+// 	},
+// 	{
+// 		title: "The Oberoi",
+// 		images: theOberoi,
+// 		category: "★ ★ ★ ★ ★",
+// 		location: "Madina",
+// 	},
+// 	{
+// 		title: "Voco",
+// 		images: voco,
+// 		category: "★ ★ ★ ★ ★",
+// 		location: "Makkah",
+// 	},
+// 	{
+// 		title: "Zowar International",
+// 		images: zowarInter,
+// 		category: "★ ★ ★ ★",
+// 		location: "Madina",
+// 	},
+// ];
